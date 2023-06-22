b620232621a8dc9098b012ebbf7c9e0cc4b2d92b
Christian Kallas <bkallas3@gmail.com>
2023-06-21 18:16:58 -0700

    added functionality for saved areas to be timestamped when weather is refetched

--- app.js
+++ app.js
@@ -8,49 +8,67 @@ const saveAreaBtn = document.getElementById('saveAreaBtn');
 const savedAreaList = document.getElementById('savedAreaList');
 
 weatherContainer.style.display = 'none';
-saveAreaBtn.style.display = "none";
+saveAreaBtn.style.display = 'none';
 
 class SelectedAreas {
-    constructor(){
+    constructor() {
         this.areas = [];
     }
     addArea(city, state) {
-        this.areas.push({city, state});
+        const area = {
+        city,
+        state,
+        lastChecked: new Date().toLocaleString(), // Store current date and time
+        };
+        this.areas.push(area);
     }
-    removeArea(city, state){
-        const index = this.areas.findIndex(savedArea => savedArea.city === city && savedArea.state === state);
-        if (index > -1) {
-            this.areas.splice(index,1);
-        }
+    removeArea(city, state) {
+        const index = this.areas.findIndex(
+        (savedArea) => savedArea.city === city && savedArea.state === state
+    );
+    if (index > -1) {
+        this.areas.splice(index, 1);
+    }
     }
     alreadySavedArea(city, state) {
-        return this.areas.some(savedArea => savedArea.city === city && savedArea.state === state);
+        return this.areas.some(
+        (savedArea) => savedArea.city === city && savedArea.state === state
+        );
     }
-    getAreas(){
-        return this.areas;
+        updateLastChecked(city, state, lastChecked) {
+        const area = this.areas.find(
+        (savedArea) => savedArea.city === city && savedArea.state === state
+        );
+        if (area) {
+        area.lastChecked = lastChecked;
+        }
+    }
+    getAreas() {
+    return this.areas;
     }
 }
 
 const selectedAreas = new SelectedAreas();
+const cityHeadings = {};
 
 locationForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    weatherContainer.style.display ='none';
+    weatherContainer.style.display = 'none';
     const location = locationInput.value.trim();
-    const [city, state] = location.split(','); 
+    const [city, state] = location.split(',');
     getWeather(city.trim(), state.trim());
 });
 
-saveAreaBtn.addEventListener('click', function(){
+saveAreaBtn.addEventListener('click', function () {
     let location = locationInput.value.trim();
-    let [city, state] = location.split (',');
+    let [city, state] = location.split(',');
     city = city.trim();
     state = state.trim();
-    
+
     if (!selectedAreas.alreadySavedArea(city, state)) {
         selectedAreas.addArea(city, state);
         updateSavedAreaList();
-    }
+    } 
     else {
         alert('This Area Is Already Saved!');
     }
@@ -60,68 +78,79 @@ function getWeather(city, state) {
     const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=1&appid=${apiKey}`;
 
     fetch(geoApiUrl)
-        .then(res => res.json())
-        .then(dataObj => {
+        .then((res) => res.json())
+        .then((dataObj) => {
             if (dataObj.length > 0) {
-                const { lat, lon } = dataObj[0];
-                getForecast(lat, lon);
-            } else {
-                messageDiv.textContent = 'Location not found.'; 
-            }
-        })
-        .catch(error => {
-            messageDiv.textContent = 'Error fetching weather data: ' + error; 
-        });
+            const { lat, lon } = dataObj[0];
+            getForecast(lat, lon, city, state); // Pass city and state to getForecast
+        } else {
+            messageDiv.textContent = 'Location not found.';
+        }
+    })
+        .catch((error) => {
+            messageDiv.textContent = 'Error fetching weather data: ' + error;
+    });
 }
 
-function getForecast(lat, lon) {
+function getForecast(lat, lon, city, state) {
     const forecastApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`;
 
     fetch(forecastApiUrl)
-        .then(res => res.json())
-        .then(dataObj => {
-            weatherTableBody.innerHTML = '';
-            messageDiv.textContent = ''; 
+        .then((res) => res.json())
+        .then((dataObj) => {
+        weatherTableBody.innerHTML = '';
+        messageDiv.textContent = '';
 
-            if (dataObj.main && dataObj.weather) {
-                const temperature = dataObj.main.temp;
-                const weatherDescription = dataObj.weather[0].description;
+        if (dataObj.main && dataObj.weather) {
+            const temperature = dataObj.main.temp;
+            const weatherDescription = dataObj.weather[0].description;
 
-                const cityName = dataObj.name; 
+            const cityName = dataObj.name;
+
+            if (!cityHeadings[cityName]) {
                 const heading = document.createElement('h1');
-                
                 heading.textContent = `Current Weather In ${cityName}`;
                 weatherContainer.insertBefore(heading, weatherContainer.firstChild);
-
-                const row = document.createElement('tr');
-                const temperatureCell = document.createElement('td');
-                const descriptionCell = document.createElement('td');
-
-                temperatureCell.textContent = `${temperature} °F`;
-                descriptionCell.textContent = weatherDescription;
-
-                row.appendChild(temperatureCell);
-                row.appendChild(descriptionCell);
-
-                weatherTableBody.appendChild(row);
-
-                weatherContainer.style.display = 'block';
-                saveAreaBtn.style.display = 'block';
-
-                const mainHeader = document.getElementById('mainHeader');
-                const locationForm = document.getElementById('locationForm');
-                
-                mainHeader.style.display = 'none';
-                locationForm.style.display = 'none';
-            } 
-            else {
-                messageDiv.textContent = 'No forecast data available for this location.';
-                saveAreaBtn.style.display = 'none';
+                cityHeadings[cityName] = true; // Mark heading as created for the city
             }
-        })
-        .catch(error => {
-            messageDiv.textContent = 'Error fetching forecast data: ' + error; 
-        });
+
+        const row = document.createElement('tr');
+        const temperatureCell = document.createElement('td');
+        const descriptionCell = document.createElement('td');
+
+        temperatureCell.textContent = `${temperature} °F`;
+        descriptionCell.textContent = weatherDescription;
+
+        row.appendChild(temperatureCell);
+        row.appendChild(descriptionCell);
+
+        weatherTableBody.appendChild(row);
+
+        weatherContainer.style.display = 'block';
+        saveAreaBtn.style.display = 'block';
+
+        const mainHeader = document.getElementById('mainHeader');
+        const locationForm = document.getElementById('locationForm');
+
+        mainHeader.style.display = 'none';
+        locationForm.style.display = 'none';
+
+        if (selectedAreas.alreadySavedArea(city, state)) {
+            selectedAreas.updateLastChecked(
+                city,
+                state,
+                new Date().toLocaleString()
+            );
+            updateSavedAreaList();
+        }
+        } else {
+            messageDiv.textContent = 'No forecast data available for this location.';
+            saveAreaBtn.style.display = 'none';
+        }
+    })
+        .catch((error) => {
+        messageDiv.textContent = 'Error fetching forecast data: ' + error;
+    });
 }
 
 function updateSavedAreaList() {
@@ -129,20 +158,20 @@ function updateSavedAreaList() {
 
     selectedAreas.getAreas().forEach((area) => {
         const listItemWrapper = document.createElement('div');
-        listItemWrapper.className = 'list-item-wrapper';
+        listItemWrapper.className = 'saved-area-item'; // Updated class name
 
         const savedAreaListItem = document.createElement('li');
-        savedAreaListItem.textContent = `${area.city}, ${area.state}`;
+        savedAreaListItem.textContent = `${area.city}, ${area.state} - Last Checked: ${area.lastChecked}`;
 
         listItemWrapper.appendChild(savedAreaListItem);
 
         const buttonWrapper = document.createElement('div');
-        buttonWrapper.className = 'button-wrapper';
+        buttonWrapper.className = 'button-container'; // Updated class name
 
         const refetchWeatherButton = document.createElement('button');
         refetchWeatherButton.className = 'btn';
-        refetchWeatherButton.textContent = 'Get Weather';
-        refetchWeatherButton.addEventListener('click', function() {
+        refetchWeatherButton.textContent = 'Check Current Weather';
+        refetchWeatherButton.addEventListener('click', function () {
             getWeather(area.city, area.state);
         });
         buttonWrapper.appendChild(refetchWeatherButton);
@@ -150,7 +179,7 @@ function updateSavedAreaList() {
         const removeAreaButton = document.createElement('button');
         removeAreaButton.className = 'btn';
         removeAreaButton.textContent = 'Remove Area';
-        removeAreaButton.addEventListener('click', function(){
+        removeAreaButton.addEventListener('click', function () {
             selectedAreas.removeArea(area.city, area.state);
             updateSavedAreaList();
         });
@@ -160,5 +189,5 @@ function updateSavedAreaList() {
         savedAreaList.appendChild(listItemWrapper);
     });
 }
-updateSavedAreaList();
 
+updateSavedAreaList();
